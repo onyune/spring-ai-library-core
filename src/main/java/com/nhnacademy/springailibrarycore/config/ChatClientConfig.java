9f931e8260ddc4f1590fe4e18aeaed45310d710c
OiJs <oijs9663@gmail.com>
2026-06-22 16:09:04 +0900

    feat: client config

---
+++ src/main/java/com/nhnacademy/springailibrarycore/config/ChatClientConfig.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.springailibrarycore.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    @Primary
+    public ChatClient.Builder ollamaChatClientBuilder(@Qualifier("ollamaChatModel")ChatModel ollamaChatModel) {
+        return ChatClient.builder(ollamaChatModel)
+                .defaultAdvisors(new SimpleLoggerAdvisor());
+    }
+
+    @Bean
+    public ChatClient.Builder geminiChatClientBuilder(@Qualifier("googleGenAiChatModel") ChatModel geminiChatModel) {
+        return ChatClient.builder(geminiChatModel)
+                .defaultAdvisors(new SimpleLoggerAdvisor());
+    }
+}
